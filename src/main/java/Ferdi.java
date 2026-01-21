c8bf8307b659cbd981f21a96d70e74e9ffaa923d
rjustyn1 <142522694+rjustyn1@users.noreply.github.com>
2026-01-22 01:10:03 +0800

    Add Mark As Done Functionality to Ferdi

--- src/main/java/Ferdi.java
+++ src/main/java/Ferdi.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Ferdi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         int taskCount = 0;
 
         String logo = " ______           _ _\n" +
@@ -29,14 +29,29 @@ public class Ferdi {
                 if (taskCount == 0) {
                     System.out.println("    You have no tasks in your list.");
                 }
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println("    " + (i + 1) + ". " + taskList[i]);
+                else {
+                    System.out.println("    Here are the tasks in your list:");
                 }
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("    " + (i + 1) + ". " + taskList[i].toString());
+                }
+            }
+            else if (command.startsWith("mark")){
+                int taskNum = Integer.parseInt(command.split(" ")[1]);
+                taskList[taskNum - 1].mark();
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("    " + taskList[taskNum - 1].toString());
+            }
+            else if (command.startsWith("unmark")){
+                int taskNum = Integer.parseInt(command.split(" ")[1]);
+                taskList[taskNum - 1].unmark();
+                System.out.println("    OK, I've marked this task as not done yet:");
+                System.out.println("    " + taskList[taskNum - 1].toString());
             }
             else {
                 System.out.println("    " + "added: " + command);
                 taskCount++;
-                taskList[taskCount - 1] = command;
+                taskList[taskCount - 1] = new Task(command);
             }
 
             System.out.println("   ____________________________________________________________\n");
@@ -44,7 +59,7 @@ public class Ferdi {
             command = scanner.nextLine();
         }
 
-        String greetEnd = "   ____________________________________________________________\n" +
+        String greetEnd = "    ____________________________________________________________\n" +
             "   Bye. Hope to see you again soon!\n" +
             "   ____________________________________________________________\n";
         
