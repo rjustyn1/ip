c439443c1b6704597a2cf2b5f3eca6760656933b
rjustyn1 <142522694+rjustyn1@users.noreply.github.com>
2026-01-22 00:06:20 +0800

    Add echo capability to Ferdi

--- src/main/java/Ferdi.java
+++ src/main/java/Ferdi.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Ferdi {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = " ______           _ _\n" +
             "|  ____|         | (_)\n" +
             "| |__ ___ _ __ __| |_ \n" +
@@ -9,13 +13,26 @@ public class Ferdi {
 
         System.out.println("Hello from\n" + logo);
 
-        String greeting = "____________________________________________________________\n" +
-            "Hello! I think I my name is Ferdi\n" +
-            "What can I do for you?\n" +
-            "____________________________________________________________\n"+
-            "Bye. Hope to see you again soon!\n" +
-            "____________________________________________________________\n";
+        String greetStart = "   ____________________________________________________________\n" +
+            "   Hello! I think I my name is Ferdi\n" +
+            "   What can I do for you?\n" +
+            "   ____________________________________________________________\n";
 
-        System.out.println(greeting);
+        System.out.println(greetStart);
+
+        String command = scanner.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + command);
+            System.out.println("    ____________________________________________________________\n");
+
+            command = scanner.nextLine();
+        }
+
+        String greetEnd = "   ____________________________________________________________\n" +
+            "   Bye. Hope to see you again soon!\n" +
+            "   ____________________________________________________________\n";
+        
+        System.out.println(greetEnd);
     }
 }
