81207d854eabaca2a84e3409a4ab96146bad82ac
rjustyn1 <142522694+rjustyn1@users.noreply.github.com>
2026-01-21 23:31:00 +0800

    Replace chatbot to Ferdi

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Ferdi User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Ferdi.java
@@ -0,0 +1,6 @@
+public class Ferdi {
+    public static void main(String[] args) {
+        String logo = "Ferdi";
+        System.out.println("Hello from\n" + logo);
+    }
+}
