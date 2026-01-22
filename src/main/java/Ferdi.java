3726ae48929cc9c25f97e63dfdf1c1916f456358
rjustyn1 <142522694+rjustyn1@users.noreply.github.com>
2026-01-23 03:28:07 +0800

    Add delete functionality to Ferdi.java

--- src/main/java/Ferdi.java
+++ src/main/java/Ferdi.java
@@ -1,10 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ferdi {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Task[] taskList = new Task[100];
-        int taskCount = 0;
+        ArrayList<Task> taskList = new ArrayList<>();
 
         String logo = " ______           _ _\n" +
             "|  ____|         | (_)\n" +
@@ -30,24 +30,23 @@ public class Ferdi {
             System.out.println("   ____________________________________________________________\n");
 
             if (parsedCommand[0].equals("list")) {
-                if (taskCount == 0) {
+                if (taskList.isEmpty()) {
                     System.out.println("    You have no tasks in your list.");
                 }
                 else {
                     System.out.println("    Here are the tasks in your list:");
                 }
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println("    " + (i + 1) + ". " + taskList[i].toString());
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println("    " + (i + 1) + ". " + taskList.get(i).toString());
                 }
             }
             else if (parsedCommand[0].equals("todo")){
                 try {
                     ToDo newTask = ToDo.createFromCommand(commandArgs);
-                    taskList[taskCount] = newTask;
-                    taskCount++;
+                    taskList.add(newTask);
                     System.out.println("    Got it. I've added this task:");
                     System.out.println("      " + newTask.toString());
-                    System.out.println("    Now you have " + taskCount + " tasks in the list.");
+                    System.out.println("    Now you have " + taskList.size() + " tasks in the list.");
                 } catch (IllegalArgumentException e) {
                     System.out.println("    " + e.getMessage());
                 }
@@ -55,11 +54,10 @@ public class Ferdi {
             else if (parsedCommand[0].equals("deadline")){
                 try {
                     Deadline newTask = Deadline.createFromCommand(commandArgs);
-                    taskList[taskCount] = newTask;
-                    taskCount++;
+                    taskList.add(newTask);
                     System.out.println("    Got it. I've added this task:");
                     System.out.println("      " + newTask.toString());
-                    System.out.println("    Now you have " + taskCount + " tasks in the list.");
+                    System.out.println("    Now you have " + taskList.size() + " tasks in the list.");
                 } catch (IllegalArgumentException e) {
                     System.out.println("    " + e.getMessage());
                 }
@@ -67,11 +65,10 @@ public class Ferdi {
             else if (parsedCommand[0].equals("event")){
                 try {
                     Event newTask = Event.createFromCommand(commandArgs);
-                    taskList[taskCount] = newTask;
-                    taskCount++;
+                    taskList.add(newTask);
                     System.out.println("    Got it. I've added this task:");
                     System.out.println("      " + newTask.toString());
-                    System.out.println("    Now you have " + taskCount + " tasks in the list.");
+                    System.out.println("    Now you have " + taskList.size() + " tasks in the list.");
                 } catch (IllegalArgumentException e) {
                     System.out.println("    " + e.getMessage());
                 }
@@ -79,32 +76,49 @@ public class Ferdi {
             else if (parsedCommand[0].equals("mark")){
                 try {
                     int taskNum = Integer.parseInt(parsedCommand[1]);
-                    taskList[taskNum - 1].mark();
+                    taskList.get(taskNum - 1).mark();
                     System.out.println("    Nice! I've marked this task as done:");
-                    System.out.println("    " + taskList[taskNum - 1].toString());
+                    System.out.println("    " + taskList.get(taskNum - 1).toString());
                 } catch (NumberFormatException e) {
                     System.out.println("    OOPS!!! Please provide a valid task number to mark.");
                 } catch (Exception e) {
-                    System.out.println("    There are only " + taskCount + " tasks in the list.");
+                    System.out.println("    There are only " + taskList.size() + " tasks in the list.");
                     System.out.println("    You cannot mark task number " + parsedCommand[1] + "." );
                 }
             }
             else if (parsedCommand[0].equals("unmark")){
                 try {
                     int taskNum = Integer.parseInt(parsedCommand[1]);
-                    taskList[taskNum - 1].unmark();
+                    taskList.get(taskNum - 1).unmark();
                     System.out.println("    OK, I've marked this task as not done yet:");
-                    System.out.println("    " + taskList[taskNum - 1].toString());
+                    System.out.println("    " + taskList.get(taskNum - 1).toString());
                 } catch (NumberFormatException e) {
                     System.out.println("    OOPS!!! Please provide a valid task number to mark.");
                 } catch (Exception e) {
-                    System.out.println("    There are only " + taskCount + " tasks in the list.");
+                    System.out.println("    There are only " + taskList.size() + " tasks in the list.");
                     System.out.println("    You cannot mark task number " + parsedCommand[1] + "." );
+                }
+            }
+            else if (parsedCommand[0].equals("delete")){
+                try {
+                    if (parsedCommand.length < 2) {
+                        throw new IllegalArgumentException("OOPS!!! Please specify which task number to delete.");
+                    }
+                    int taskNum = Integer.parseInt(parsedCommand[1]);
+                    Task removedTask = taskList.remove(taskNum - 1);
+                    System.out.println("    Noted. I've removed this task:");
+                    System.out.println("      " + removedTask.toString());
+                    System.out.println("    Now you have " + taskList.size() + " tasks in the list.");
+                } catch (NumberFormatException e) {
+                    System.out.println("    OOPS!!! Please provide a valid task number to delete.");
+                } catch (Exception e) {
+                    System.out.println("    There are only " + taskList.size() + " tasks in the list.");
+                    System.out.println("    You cannot delete task number " + parsedCommand[1] + "." );
                 }
             }
             else {
                 System.out.println("    " + "Unknown command: " + parsedCommand[0]);
-                System.out.println("    " + "Please try again, with \"todo\", \"deadline\", \"event\", \"mark\", \"unmark\", \"list\", or \"bye\".");
+                System.out.println("    " + "Please try again, with \"todo\", \"deadline\", \"event\", \"mark\", \"unmark\", \"delete\", \"list\", or \"bye\".");
             }
 
             System.out.println("   ____________________________________________________________\n");
