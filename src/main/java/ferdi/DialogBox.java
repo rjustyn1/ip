653f205ad84f7152782794b0e23ffcf6d49acec5
rjustyn1 <142522694+rjustyn1@users.noreply.github.com>
2026-02-05 17:18:13 +0800

    Prettify GUI with FXML and CSS

--- src/main/java/ferdi/DialogBox.java
+++ src/main/java/ferdi/DialogBox.java
@@ -1,6 +1,13 @@
 package ferdi;
 
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -8,19 +15,54 @@ import javafx.scene.layout.HBox;
 
 public class DialogBox extends HBox {
 
-    private final Label text;
-    private final ImageView displayPicture;
+    private static final String USER_DIALOG_STYLE = "user-dialog";
+    private static final String FERDI_DIALOG_STYLE = "ferdi-dialog";
 
-    public DialogBox(String s, Image i) {
-        text = new Label(s);
-        displayPicture = new ImageView(i);
+    @FXML
+    private Label text;
+    @FXML
+    private ImageView displayPicture;
 
-        //Styling the dialog box
+    private DialogBox(String s, Image i) {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/DialogBox.fxml"));
+        fxmlLoader.setController(this);
+        fxmlLoader.setRoot(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to load DialogBox.fxml", e);
+        }
+
+        text.setText(s);
+        displayPicture.setImage(i);
         text.setWrapText(true);
-        displayPicture.setFitWidth(100.0);
-        displayPicture.setFitHeight(100.0);
-        this.setAlignment(Pos.TOP_RIGHT);
 
-        this.getChildren().addAll(text, displayPicture);
+        this.getStyleClass().add("dialog-box");
+        text.getStyleClass().add("dialog-text");
+        displayPicture.getStyleClass().add("dialog-image");
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogBox getUserDialog(String s, Image i) {
+        DialogBox dialogBox = new DialogBox(s, i);
+        dialogBox.getStyleClass().add(USER_DIALOG_STYLE);
+        return dialogBox;
+    }
+
+    public static DialogBox getFerdiDialog(String s, Image i) {
+        DialogBox dialogBox = new DialogBox(s, i);
+        dialogBox.flip();
+        dialogBox.getStyleClass().add(FERDI_DIALOG_STYLE);
+        return dialogBox;
     }
 }
--- src/main/java/ferdi/Main.java
+++ src/main/java/ferdi/Main.java
@@ -1,98 +1,36 @@
 package ferdi;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private ScrollPane scrollPane;
-    private VBox dialogContainer;
-    private TextField userInput;
-    private Button sendButton;
-    private Scene scene;
-
-    private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private final Image ferdiImage = new Image(this.getClass().getResourceAsStream("/images/DaFerdi.png"));
-    private final Ferdi ferdi = new Ferdi("./src/main/java/data/ferdi.txt");
-
+    private static final String DATA_PATH = "./src/main/java/data/ferdi.txt";
 
     @Override
     public void start(Stage stage) {
-         //Setting up required components
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+        AnchorPane root;
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to load MainWindow.fxml", e);
+        }
 
-         scrollPane = new ScrollPane();
-         dialogContainer = new VBox();
-         scrollPane.setContent(dialogContainer);
-
-         userInput = new TextField();
-         sendButton = new Button("Send");
-
-        AnchorPane mainLayout = new AnchorPane();
-        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
-
-        // Layout sizing
-        mainLayout.setPrefSize(400.0, 600.0);
-
-        scrollPane.setPrefSize(385, 535);
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-        scrollPane.setFitToWidth(true);
-
-        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
-
-        userInput.setPrefWidth(325.0);
-        sendButton.setPrefWidth(55.0);
-
-        // Anchor placements
-        AnchorPane.setTopAnchor(scrollPane, 1.0);
-        AnchorPane.setLeftAnchor(scrollPane, 1.0);
-        AnchorPane.setRightAnchor(scrollPane, 1.0);
-
-        AnchorPane.setBottomAnchor(userInput, 1.0);
-        AnchorPane.setLeftAnchor(userInput, 1.0);
-
-        AnchorPane.setBottomAnchor(sendButton, 1.0);
-        AnchorPane.setRightAnchor(sendButton, 1.0);
-
-        scene = new Scene(mainLayout);
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(Main.class.getResource("/view/styles.css").toExternalForm());
 
         stage.setTitle("Ferdi");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
 
-        // Handling user input
-        sendButton.setOnMouseClicked((event) -> {
-            handleUserInput();
-        });
-        userInput.setOnAction((event) -> {
-            handleUserInput();
-        });
-
-        // Scroll down to the end every time dialogContainer's height changes.
-        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
-    }
-
-    /**
-     * Creates a dialog box containing user input, and appends it to
-     * the dialog container. Clears the user input after processing.
-     */
-    private void handleUserInput() {
-        String userText = userInput.getText();
-        String ferdiText = ferdi.getResponse(userText);
-        dialogContainer.getChildren().addAll(
-                new DialogBox(userText, userImage),
-                new DialogBox(ferdiText, ferdiImage)
-        );
-        userInput.clear();
+        MainWindow controller = fxmlLoader.getController();
+        controller.setFerdi(new Ferdi(DATA_PATH));
     }
 }
