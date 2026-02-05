40c237471e73725464819a0d2e5057332e2e30e9
rjustyn1 <142522694+rjustyn1@users.noreply.github.com>
2026-02-05 11:53:12 +0800

    Add chat box

--- src/main/java/ferdi/Main.java
+++ src/main/java/ferdi/Main.java
@@ -2,20 +2,71 @@ package ferdi;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-/**
- * Simple JavaFX entry point showing a "Hello World" label.
- */
+
 public class Main extends Application {
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    // Images will be loaded later when needed
+    // private Image userImage = new Image(this.getClass().getResourceAsStream("resources/images/DaUser.png"));
+    // private Image dukeImage = new Image(this.getClass().getResourceAsStream("resources/images/DaDuke.png"));
+
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!");
-        Scene scene = new Scene(helloWorld);
+         //Setting up required components
 
+         scrollPane = new ScrollPane();
+         dialogContainer = new VBox();
+         scrollPane.setContent(dialogContainer);
+
+         userInput = new TextField();
+         sendButton = new Button("Send");
+
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        // Layout sizing
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+        sendButton.setPrefWidth(55.0);
+
+        // Anchor placements
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+        AnchorPane.setLeftAnchor(scrollPane, 1.0);
+        AnchorPane.setRightAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        scene = new Scene(mainLayout);
+
+        stage.setTitle("Ferdi");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 }
